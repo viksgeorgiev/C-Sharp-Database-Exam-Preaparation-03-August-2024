f92a6119379a9fd146cb4b32ad553e71993801ee
Viks Georgiev <viks47@gmail.com>
2025-03-22 09:52:49 +0200

    Add project files.

---
+++ TravelAgency/DataProcessor/Deserializer.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TravelAgency.Data;
+
+namespace TravelAgency.DataProcessor
+{
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data format!";
+        private const string DuplicationDataMessage = "Error! Data duplicated.";
+        private const string SuccessfullyImportedCustomer = "Successfully imported customer - {0}";
+        private const string SuccessfullyImportedBooking = "Successfully imported booking. TourPackage: {0}, Date: {1}";
+
+        public static string ImportCustomers(TravelAgencyContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportBookings(TravelAgencyContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validateContext, validationResults, true);
+
+            foreach (var validationResult in validationResults)
+            {
+                string currValidationMessage = validationResult.ErrorMessage;
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ TravelAgency/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using TravelAgency.Data;
+
+namespace TravelAgency.DataProcessor
+{
+    public class Serializer
+    {
+        public static string ExportGuidesWithSpanishLanguageWithAllTheirTourPackages(TravelAgencyContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportCustomersThatHaveBookedHorseRidingTourPackage(TravelAgencyContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
